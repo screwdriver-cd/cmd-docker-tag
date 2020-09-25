7ef1434ceb015d282c98f698802c50af13b844c8
Jithin Emmanuel <jithine@verizonmedia.com>
2020-09-25 14:58:38 -0700

    feat: adding docker-trigger command

---
+++ run.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/screwdriver-cd/toolbox.git ci
+git fetch
+export DOCKER_TAG=`git describe --tags`
+./ci/docker-trigger.sh
+meta set docker_tag $DOCKER_TAG && meta get docker_tag
